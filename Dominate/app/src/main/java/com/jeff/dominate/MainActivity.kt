ce8592ebc74f8bc1119508eed58d89fd5e364de6
JEFF <yaojie4433520@qq.com>
2018-12-13 00:56:18 +0800

    /**  * author : Jeff  5899859876@qq.com  * Csdn :https://blog.csdn.net/Jeff_YaoJie  * Github: https://github.com/Jay-YaoJie  * Created :  2018-12-13.  * description ：main  */

--- Dominate/app/src/main/java/com/jeff/dominate/MainActivity.kt
+++ Dominate/app/src/main/java/com/jeff/dominate/MainActivity.kt
@@ -4,6 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * author : Jeff  5899859876@qq.com
+ * Csdn :https://blog.csdn.net/Jeff_YaoJie
+ * Github: https://github.com/Jay-YaoJie
+ * Created :  2018-12-13.
+ * description ：main
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
