e22b763ec3f4c5832af4f4ae60bf06ee2fcd05f7
jeff <yaojie4433520@qq.com>
2019-01-09 10:26:12 +0800

    STYLE_IOS

--- Dominate/mylibrary/src/main/kotlin/jeff/bases/DominateApplication.kt
+++ Dominate/mylibrary/src/main/kotlin/jeff/bases/DominateApplication.kt
@@ -13,13 +13,34 @@ import com.kongzue.dialog.v2.DialogSettings
  */
 class DominateApplication {
     companion object {
-        lateinit var instance:Application;
+        lateinit var instance: Application;
         //初始化全局的风格样式 dialog
-        fun dialogSetting(){
-            //iOS 风格对应 DialogSettings.STYLE_IOS
+        fun dialogSetting() {
+            
+            //是否打印日志
+            // public static boolean DEBUGMODE = true;
+            DialogSettings.DEBUGMODE = true
+
+            //决定等待框、提示框以及iOS风格的对话框是否启用模糊背景
+            //public static boolean use_blur = true;
+
+            //决定等待框、提示框以及iOS风格的对话框的模糊背景透明度（50-255）
+            // public static int blur_alpha = 200;
+
+            //决定对话框的默认样式，请使用 TYPE_MATERIAL、TYPE_KONGZUE、TYPE_IOS 赋值
+            //  public static int type = 0;
             DialogSettings.style = DialogSettings.STYLE_IOS
-            //设置提示框主题为亮色主题
-            DialogSettings.tip_theme = DialogSettings.THEME_LIGHT;
+
+            //决定对话框的模式（亮色和暗色两种），请使用 THEME_LIGHT、THEME_DARK 赋值
+            //public static int dialog_theme = 0;
+            DialogSettings.dialog_theme = DialogSettings.THEME_LIGHT
+
+            //决定对话框的默认背景色
+            // public static int dialog_background_color = -1;
+
+            //决定提示框的模式（亮色和暗色两种），请使用 THEME_LIGHT、THEME_DARK 赋值
+            //public static int tip_theme = 1;
+            DialogSettings.tip_theme = DialogSettings.THEME_LIGHT
         }
     }
 
